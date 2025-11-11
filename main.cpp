50ebaea79016807e214435bad96f5d1301e1fcf8
Salaheldin Mahmoud <salahmahmoud3377@gmail.com>
2025-11-11 09:36:27 +0200

    Implement course management with instructor and students
    Added instructor and student creation, and course management.

--- main.cpp
+++ main.cpp
@@ -33,8 +33,45 @@ int main() {
 
    
     cout << "-----------------------------------" << endl;
+    cout << "Course" << endl;
+    #include <iostream>
+#include "Person.h"
+#include "Student.h"
+#include "Instructor.h"
+#include "Course.h"
+using namespace std;
+
+int main() {
+    // Create an instructor
+    Instructor instructor1("Dr.Lina Khaled", 101, "Computer Science", 5);
+
+    // Create some students
+    Student s1("Salah", 2025, 2, "Computer Science");
+    Student s2("Omar", 2026, 1, "Software Engineering");
+    Student s3("Laila", 2027, 1, "Data Science");
+
+    // Create a course
+    Course course1("CS101", "Introduction to Programming", 3);
+
+    // Add students to the course
+    course1.addStudent(s1);
+    course1.addStudent(s2);
+    course1.addStudent(s3);
+
+    // Display information
+    cout << "-----------------------------------" << endl;
     cout << "Instructor Information:" << endl;
     instructor1.display();
+
+    cout << "\n-----------------------------------" << endl;
+    cout << "Course Information:" << endl;
+    course1.displayCourseInfo();
+
+    cout << "-----------------------------------" << endl;
+
+    return 0;
+}
+.display();
 
     cout << "\n-----------------------------------" << endl;
     cout << "Course Information:" << endl;

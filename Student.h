c24ddd5d4405d0921d7f8b1c6eeda57d636b3226
Salaheldin Mahmoud <salahmahmoud3377@gmail.com>
2025-11-11 09:24:35 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,24 +1,60 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include "Person.h"
+#include <iostream>
 #include <string>
-#using namespace std;
-//#write student class here
-class Student :: Person {
-private int yearLevel;
-private string major;
-display(){
-
-}
+#include "Person.h"
+using namespace std;
 
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
+
+public:
+ 
+    Student(string n = "", int i = 0, int year = 1, string m = "")
+        : Person(n, i) { 
+        yearLevel = year;
+        major = m;
+    }
+
+ 
+    int getYearLevel() const {
+        return yearLevel;
+    }
+
+    string getMajor() const {
+        return major;
+    }
+
+    
+    void setYearLevel(int year) {
+        yearLevel = year;
+    }
+
+    void setMajor(string m) {
+        major = m;
+    }
 
 
+    void display() const {
+       
+        Person::display();
 
-
-
-
-}
+        
+        cout << "Year Level: " << yearLevel << endl;
+        cout << "Major: " << major << endl;
+    }
+};
 
 #endif
+
+
+
+
+
+
+
+

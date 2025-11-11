028b4b77af6a8f9e4372b04de9376ed5f678d599
Salaheldin Mahmoud <salahmahmoud3377@gmail.com>
2025-11-11 09:18:54 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,26 +1,32 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
 
+#include <iostream>
+#include <string>
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-class instructor :: Person {
-private string department ;
-private int experienceYears;
 
 
-display(){
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
 
+public:
+  
+    Instructor(string name, int age, string dept, int years)
+        : Person(name, age) {  
+        department = dept;
+        experienceYears = years;
+    }
 
-}
-
-
-
-
-
-
-
-
-
+    
+    void display() const {
+        cout << "Instructor Name: " << getName() << endl;
+        cout << "Age: " << getAge() << endl;
+        cout << "Department: " << department << endl;
+        cout << "Years of Experience: " << experienceYears << endl;
+    }
+};
 
 #endif
